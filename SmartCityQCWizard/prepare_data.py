9ff6565d5108ff66a71dd85dbeb95b51e2361b20
MatteoRobbiati <robbiati.matteo@gmail.com>
2024-10-25 10:22:58 +0200

    restore node 000

--- SmartCityQCWizard/prepare_data.py
+++ SmartCityQCWizard/prepare_data.py
@@ -1,9 +1,7 @@
 import pandas as pd
 
 
-zones = [f"{i:03}" for i in range(1, 13, 1)]
-
-datetime = "2024-08-01 23:45:00"
+zones = [f"{i:03}" for i in range(0, 13, 1)]
 
 def zones_data_by_datetime(df, datetime):
     filtered_df = df[df["datetime"] == datetime]
@@ -11,8 +9,6 @@ def zones_data_by_datetime(df, datetime):
     zones_data = {}
     for zone in zones:
         value = filtered_df.loc[filtered_df["areaAnalisi"] == f"Cagliari - {zone}", "value"].values
-        # shifting the label from "003" to "002" 
-        new_zone_number = int(zone) - 1
-        zones_data.update({f"{new_zone_number:03}" : value[0]})
+        zones_data.update({zone : value[0]})
     return zones_data
 
